b8666347b4ca1a882e01080ea5e4a7990829cc7c
shino022 <102046920+shino022@users.noreply.github.com>
2022-12-10 18:33:16 -0500

    Add functions to handle get all roomchats and post a chat

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -5,11 +5,12 @@ const router = express.Router();
 const microPostsController = require('./microPosts.js');
 const authController = require('./auth.js');
 const roomController = require('./rooms.js');
-
+const roomChatsController = require('./roomChats.js');
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
 router.use('/auth', authController);
 router.use('/micro_posts', microPostsController);
 router.use('/rooms', roomController);
+router.use('/room_chats', roomChatsController);
 
 module.exports = router;

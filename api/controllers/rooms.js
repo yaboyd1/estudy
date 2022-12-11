e0e502f7df4c781ddb43fa7ad641909da4ca6ec2
shino022 <102046920+shino022@users.noreply.github.com>
2022-12-10 20:54:25 -0500

    Add a function to handle room delete request

--- api/controllers/rooms.js
+++ api/controllers/rooms.js
@@ -3,15 +3,14 @@ const passport = require('../middlewares/authentication');
 const router = express.Router();
 const db = require('../models');
 
-const { Room, User } = db;
+const { Room, User, RoomChat } = db;
 
 // This is a simple example for providing basic CRUD routes for
 // a resource/model. It provides the following:
 //    GET    /api/room
 //    POST   /api/room
 //    GET    /api/room/:id
-//    PUT    /api/room/:id
-//    DELETE /api/room/:id
+//    DELETE /api/room/
 //
 // The full URL's for these routes are composed by combining the
 // prefixes used to load the controller files.
@@ -36,7 +35,7 @@ router.post('/', passport.isAuthenticated(), (req, res) => {
       User.update(
         {
           roomId: newRoom.id,
-          role: 'founder',
+          role: 'admin',
         },
         {
           where: {
@@ -62,35 +61,41 @@ router.get('/:id', (req, res) => {
   });
 });
 
-router.put('/:id', passport.isAuthenticated(), (req, res) => {
-  const { id } = req.params;
-  Room.findByPk(id).then((room) => {
+// @desc    Delete all chats, update user's role and delete the room
+// @route   DELETE /api/rooms
+// @access  Private
+router.delete('/', passport.isAuthenticated(), async (req, res) => {
+  try {
+    const user = await User.findByPk(req.user.id);
+    const room = await Room.findByPk(user.roomId);
+
     if (!room) {
       return res.sendStatus(404);
     }
 
-    room.content = req.body.content;
-    room
-      .save()
-      .then((updatedRoom) => {
-        res.json(updatedRoom);
-      })
-      .catch((err) => {
-        res.status(400).json(err);
-      });
-  });
-});
+    await RoomChat.destroy({
+      where: {
+        roomId: room.id
+      }
+    });
+    
+    await User.update(
+      {
+        role: null
+      },
+      {
+        where: {
+          roomId: room.id        
+        }
+      }
+    );
 
-router.delete('/:id', passport.isAuthenticated(), (req, res) => {
-  const { id } = req.params;
-  Room.findByPk(id).then((room) => {
-    if (!room) {
-      return res.sendStatus(404);
-    }
+    await room.destroy();
 
-    room.destroy();
     res.sendStatus(204);
-  });
+  } catch (err) {
+    return res.status(400).json(err);
+  }
 });
 
 module.exports = router;

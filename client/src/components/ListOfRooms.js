578167749c09a55276f28d9fe08ac227d247a709
shino022 <102046920+shino022@users.noreply.github.com>
2022-12-12 10:06:52 -0500

    Add a click link handler when a user enters a room

--- client/src/components/ListOfRooms.js
+++ client/src/components/ListOfRooms.js
@@ -2,11 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function ListOfRooms({ name, createdAt, id }) {
+  const onClickLink = async () => {
+    try {
+      let response = await fetch(`/api/rooms/${id}`, {
+        method: 'put',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          action: 'enter',
+        }),
+      });
+      console.log(response);
+    } catch (error) {
+    }
+  }
   return (
     <div className="col-10 col-md-8 col-lg-7">
       <div className="card mb-4 shadow">
         <div className="card-body card-text">
-          <Link to={'/room/' + name}>{name}</Link>
+          <Link onClick={onClickLink} to={'/room/' + name}>{name}</Link>
         </div>
         <div className="card-footer small text-muted text-end">{createdAt}</div>
       </div>
